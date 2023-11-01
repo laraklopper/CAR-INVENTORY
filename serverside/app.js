bd607e49ca9be730632ce294aa13d44cd6870fcd
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-01 15:05:04 +0200

    Update app.js

--- serverside/app.js
+++ serverside/app.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require('express');// Import Express Web framework for handling HTTP requests
+const mongoose = require('mongoose');//Import the Mongoose library
 const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const carController = require('./controllers/carControllers');
-
+const carController = require('./controllers/carControllers');// Import the car controller module
+require('dotenv').config();// Load environment variables from .env file
 const database = process.env.DATABASE_NAME;
 const uri = process.env.DATABASE_URL;
 const port = process.env.PORT || 3001;
 const app = express();
 
+//=================CHECK IF ALL THE ENVIRONMENTAL VARIABLES A PRESENT==================
 if (!port) {
     console.error('Error: PORT environment variable is missing');
     process.exit(1);
@@ -26,11 +26,12 @@ if (!uri) {
 
 mongoose.Promise = global.Promise;
 
+//================================================================
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    useCreateIndex: true, // Added for better index support
+    useCreateIndex: true, 
     dbName: database
 })
 .then(() => {
@@ -41,6 +42,8 @@ mongoose.connect(uri, {
     process.exit(1);
 });
 
+//==============MONGO CONNECTION EVENT HANDLERS========================
+
 mongoose.connection.on('error', function (error) {
     console.log('Could not connect to the database. Exiting now...', error);
     process.exit(1);
@@ -50,13 +53,18 @@ mongoose.connection.once('open', function () {
     console.log('Successfully connected to database');
 });
 
+//=============SETUP MIDDLEWARE===================
 app.use(cors());
-app.use(express.json());
+app.use(express.json());// Middleware to parse JSON in the request body
+
+//================API ROUTES=============================
+// API routes using the controller functions
 
 app.post('/addCar', carController.addCar);
 app.get('/findAllCars', carController.findAllCars);
-app.put('/updateCarById/:make', carController.updateById); // Added a parameter in the route
+app.put('/updateCarById/:make', carController.updateById); 
 
+//==================START THE SERVER========================
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
