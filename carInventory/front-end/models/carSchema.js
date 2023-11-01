01fba494048e9661facd8dcf14d767b86303e4f8
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-01 14:49:56 +0200

    Update carSchema.js

--- carInventory/front-end/models/carSchema.js
+++ carInventory/front-end/models/carSchema.js
@@ -1,1 +1,24 @@
+const mongoose = require('mongoose');
 
+let carSchema = mongoose.Schema({
+    make: {
+        type: String,
+        required: true,
+    },
+    model
+
+: {
+        type: String,
+        required: true,
+    },
+    registration: {
+        type: String,
+        required: true,
+    },
+    owner: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('carInventory', carSchema);
