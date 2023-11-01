450d7e2db7a18d580806d44f8930ccddf805b022
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-01 15:05:37 +0200

    Add files via upload

---
+++ carInventory/app.js
@@ -0,0 +1,76 @@
+const express = require('express');// Import Express Web framework for handling HTTP requests
+const mongoose = require('mongoose');//Import the Mongoose library
+const cors = require('cors');// Middleware for enabling Cross-Origin Resource Sharing
+const carController = require('./controllers/carControllers')// Import the car controller module
+require('dotenv').config(); // Load environment variables from .env file
+//Extract Environmental Variables
+const database = process.env.DATABASE_NAME;//Extract the database name from enviromental variables
+const uri = process.env.DATABASE_URL;//Extract the database URL from the enviromental variables
+const app = express();// Create an Express application
+const port = process.env.PORT || 3001;// Extract the port number from environment variables
+
+
+//=================CHECK IF ALL THE ENVIRONMENTAL VARIABLES A PRESENT==================
+if (!port) {
+    console.error('Error: PORT environment variable is missing');
+    process.exit(1);
+}
+
+if (!database) {
+    console.error('Error: DATABASE_NAME environment variable is missing');
+    process.exit(1);
+}
+
+if (!uri) {
+    console.error('Error: DATABASE_URL environment variable is missing');
+    process.exit(1);
+}
+
+mongoose.Promise = global.Promise;// Set Mongoose to use Node's built-in promises
+
+//==============CONNECT TO MONGODB USING MONGOOSE=======================
+
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,         // Use the new URL parser
+    useUnifiedTopology: true,    // Use the new Server Discover and Monitoring engine
+    useFindAndModify: false,  // Opt out of using the deprecated findAndModify function
+    useCreateIndex: true,
+    dbName: database            // Specify the name of the MongoDB database
+})
+    .then(() => {//Execute when the connection is successful.
+        console.log('Connected to MongoDB');//Log a success message when the connection is established.
+    })
+    .catch((err) => {
+        // Connection error
+        console.error('Error connecting to MongoDB', err);//Log an error message if there's an issue connecting to MongoDB.
+    });
+
+
+//==============MONGO CONNECTION EVENT HANDLERS========================
+// Set up an event listener for the 'error' event on the Mongoose connection
+mongoose.connection.on('error', function (error) {// This function will be executed when there is an error in the MongoDB connection
+    console.log('Could not connect to the database. Exiting now...', error);// Log an error message with details about the connection failure
+    process.exit(1);// Terminate the Node.js process with an exit code of 1, indicating an error
+});
+
+
+// Set up an event listener for the 'open' event on the Mongoose connection
+mongoose.connection.once('open', function () {// This function will be executed when the MongoDB connection is successfully opened
+    console.log('Successfully connected to database');// Log a message indicating that the connection to the database was successful
+});
+
+
+app.use(express.json());// Middleware to parse JSON in the request body
+app.use(cors());
+
+// API routes using the controller functions
+app.post('/addCar', carController.addCar);
+app.get('/findAllCars', carController.findAllCars);
+app.put('/updateCarById/:make', carController.updateById); 
+
+//================START THE SERVER=========================
+// Start the Express server and listen on the specified port
+app.listen(port, () => {// Start the Express server and listen on the specified port.
+    console.log(`Server is running on ${port}`);// Log a message indicating that the server is running on the specified port.
+});
