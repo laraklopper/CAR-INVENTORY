0e9bb4242ddb2e8a4244274fb5a5785a13a8761c
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-05 16:44:10 +0200

    Update UpdateForm.js

--- carInventory/front-end/src/components/UpdateForm.js
+++ carInventory/front-end/src/components/UpdateForm.js
@@ -1,70 +1,78 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-export default function UpdateForm() {
+//UpdateForm function component
+export default function UpdateForm() {//Export default update form function component
+    const [newMake, setNewMake] = useState('');
+    const [newModel, setNewModel] = useState('');
+    const [newRegistration, setNewRegistration] = useState('');
+    const [newOwner, setNewOwner] = useState('');
+
+    //==================JSX RENDERING==================
   return (
-
           <form
-       //   onSubmit={updateCarDetails}
-          >
+          className='updateForm'
+        //   onSubmit={(event) => { event.preventDefault(); updateCarDetails() }
+                    >
+            <Row>
             
-                  <Col>
+                  <Col className='updateCol'>
                       <label className=''>
                         <p className='labelText'>MAKE:</p>
                           <input
                               type='text'
-                            // onChange={(e) => setNewMake(e.target.value)}
-                            // value={newMake}
+                            onChange={(e) => setNewMake(e.target.value)}
+                            value={newMake}
                             className='changeInput'
                           />
                       </label>
                   </Col>
-                  <Col>
+                  <Col className='updateCol'>
                       <label className='label'>
                   <p className='labelText'>REGISTRATION:</p>
                     <input 
                       type='text'
-                    //   onChange={(e) => setNewRegistration(e.target.value)}
-                    //   value={newRegistration}/
+                      onChange={(e) => setNewRegistration(e.target.value)}
+                      value={newRegistration}
                       className='changeInput'        
                           />
                       </label>
                   </Col>
     
-                  <Col>
+                  <Col className='updateCol'>
                   <label className='updateLabel'>
                       <p className='labelText'>MODEL:</p>
                       <input
                         type='text'
-                        // onChange={(e) => setNewModel(e.target.value)}
-                        // value={newModel}
+                        onChange={(e) => setNewModel(e.target.value)}
+                        value={newModel}
                         className='changeInput'
                          />
                   </label>
                   
                   </Col>
-                  <Col>
+                  <Col className='updateCol'>
                   <label className='updateLabel'>
                       <p className='labelText'>OWNER:</p>
                       <input 
                       type='text'
-                      //onChange={(e) => setNewOwner(e.target.value)}
-                    //  value={newOwner}
-                      className='changeInput'
-                      
+                      onChange={(e) => setNewOwner(e.target.value)}
+                     value={newOwner}
+                      className='changeInput'                      
                       />
                   </label>
-                  </Col>
-                
+                  </Col>        
+          </Row>
+          <Row>
               <Col>
                   <Button type='submit' variant="success">UPDATE CAR DETAILS</Button>
 
               </Col>
+          </Row>
               </form>
      
     
   )
 }
-
-
