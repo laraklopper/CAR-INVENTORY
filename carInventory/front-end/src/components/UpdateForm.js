7579bb200697a19abee0ccdbb783d9b757352660
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-05 11:59:12 +0200

    Update UpdateForm.js

--- carInventory/front-end/src/components/UpdateForm.js
+++ carInventory/front-end/src/components/UpdateForm.js
@@ -1,1 +1,28 @@
+import React from 'react'
+
+export default function UpdateForm() {
+  return (
+    <div>
+        <form>
+            <label className=''>
+                  
+                <input/>
+            </label>
+            <label>
+                MODEL:
+                <input/>
+            </label>
+              <label>
+                REGISTRATION
+                  <input />
+              </label>
+              <label>
+                OWNER:
+                  <input />
+              </label>
+
+        </form>
+    </div>
+  )
+}
 
