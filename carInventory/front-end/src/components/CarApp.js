66ff5343bfffc14dd5a87843868e4a7afd9137ee
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-06 15:20:54 +0200

    Update CarApp.js

--- carInventory/front-end/src/components/CarApp.js
+++ carInventory/front-end/src/components/CarApp.js
@@ -3,8 +3,9 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Header from './components/Header';
 import Button from 'react-bootstrap/Button';
+import Header from './components/Header';
+import CarForm from './components/CarForm';
 import UpdateForm from './components/UpdateForm';
 
 export default function App() {
@@ -132,62 +133,7 @@ export default function App() {
           <form id='form' onSubmit={addCar}>
             <Row>
               <Col className='formCol'>
-                <label className='formLabel'>
-                  <p className='labelText'>MAKE:</p>
-                  <input
-                    type='text'
-                    className='formInput'
-                    name='make'
-                    value={carData.make}
-                    onChange={handleInputChange}
-                  />
-                </label>
-              </Col>
-              <Col className='formCol'>
-                <label className='formLabel'>
-                  <p className='labelText'>MODEL:</p>
-                  <input
-                    className='formInput'
-                    type='text'
-                    name='model'
-                    value={carData.model}
-                    onChange={handleInputChange}
-                  />
-                </label>
-              </Col>
-              <Col className='formCol'>
-                <label className='formLabel'>
-                  <p className='labelText'>REGISTRATION:</p>
-                  <input
-                    className='formInput'
-                    type='text'
-                    name='registration'
-                    value={carData.registration}
-                    onChange={handleInputChange}
-                  />
-                </label>
-              </Col>
-              <Col className='formCol'>
-                <label className='formLabel'>
-                  <p className='labelText'>OWNER:</p>
-                  <input
-                    className='formInput'
-                    type='text'
-                    name='owner'
-                    value={carData.owner}
-                    onChange={handleInputChange}
-                  />
-                </label>
-              </Col>
-            </Row>
-            <Row>
-              <Col className='col'>
-                <Button variant="primary" id='submitBtn' type='submit'>
-                  ADD CAR
-                </Button>
-              </Col>
-            </Row>
-          </form>
+                          <CarForm carData={carData} handleInputChange={handleInputChange} addCar={addCar} />
         </section>
         <section id='section2'>
           <Row>
