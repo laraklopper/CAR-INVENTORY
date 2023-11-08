72cce0eb05f051df34d284b4d2f5c9cfb46e2c2f
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-08 16:26:30 +0200

    Update carControllers.js

--- carInventory/controllers/carControllers.js
+++ carInventory/controllers/carControllers.js
@@ -1,32 +1,34 @@
 const Car = require('../models/carSchema');// Import the Car model representing the structure of the document in the MongoDB collection
 
 // Controller function to create a new car
-const addCar = async function (req, res) {//Define an async function to add a new car
-  try {
-    const newCar = new Car(req.body);      // Create a new Car instance with the request body
-    await newCar.save();    // Save the new car to the database
-    //The save method is called on the new instance to save it to the MongoDB database.
+const addCar = async function (req, res) {//Define an async function to add a new car 
+    try {
+        const newCar = new Car(req.body);// Create a new Car instance with the request body and save it to the database
+        await newCar.save(); // Save the new car to the database
 
-    res.status(201).json(newCar);    // Respond with the newly created car and a 201 status code (Created)
+        res.status(201).json(newCar);// Respond with the newly created car and a 201 status code (Created)
 
-  } catch (error) {
-      //Handle Errors
-    console.error('Error adding the new car', error.message);//Log an error message in the console for debugging purposes.
-    res.status(500).send('Internal server error');// Respond with a 500 status code, indicating an internal server error, and with a generic error message.
-  }
+    } 
+    catch (error) {
+        // Handle errors 
+        console.error('Error adding the new car', error.message);//Log an error message in the console for debugging purposes.
+        res.status(500).send('Internal server error');//Send a 500 status code and an error message
+    }
 };
 
 // Controller function to retrieve all cars
 const findAllCars = async function (req, res) {//Define an aysnc function to fetch all cars from the database
-  try {
-    const cars = await Car.find();//Fetch all cars from the database
-    res.json(cars);//Respond with the list of cars
-  } 
-  catch (error) {
-      //Handle errors
-    console.error('Error fetching cars', error.message);//Log an error message in the console for debugging purposes.
-    res.status(500).send('Internal server Error');//Send a 500 status code, and an error message
-  }
+    try {
+        // Fetch all cars from the database based on any optional query parameters in req.body
+        const cars = await Car.find(req.body);
+        res.json(cars);// Respond with the list of cars
+    } 
+    
+    catch (error) {
+        // Handle errors 
+        console.error('Error fetching cars', error.message);//Log an error message in the console for debugging purposes.
+        res.status(500).send('Internal server Error');//Sending a 500 status code, and an error message
+    }
 };
 
 // Controller function to update one single car by Id
@@ -47,7 +49,7 @@ const updateById = async (req, res) => {//Define an async function to update a s
         }
 
         res.json(updatedCar);// Respond with the updated car
-        console.log(updatedCar)
+        console.log(updatedCar);//Log the updated car to the console
 
     } 
     catch (error) {
@@ -57,10 +59,11 @@ const updateById = async (req, res) => {//Define an async function to update a s
     }
 };
 
-// Controller function to remove a single car by Id
+
+//Controller function to remove one car
 const removeById = async (req, res) => {//Define an async function to remove a car from the database
     const  _id  = req.params._id;//Extract the make parameter from the URL
-    // const carId = req.params.carId
+
     console.log(_id + "Remove by Id");
     try {
         //Find and remove a car
@@ -68,23 +71,52 @@ const removeById = async (req, res) => {//Define an async function to remove a c
 
         //Conditional rendering to check if the car is found
         if (!removedCar) {
-            return res.status(404).json({ error: 'Car not found' });//Respond with a 404 status code and a message indicating the car is not found
+            return res.status(404).json({ error: 'Car not found' })
+        //Respond with a 404 status code and a message indicating the car is not found
         }
 
         res.json({ message: 'Car removed successfully' });//Respond with a JSON object containing a success message
 
     } catch (error) {
         //Handle errors
-        console.error('Error removing car', error.message);        // Handle errors during the removal process
-        res.status(500).send('Internal server Error');        // Handle errors during the removal process
+        console.error('Error removing car', error.message); // Handle errors during the removal process
+        res.status(500).send('Internal server Error');   // Handle errors during the removal process
 
     }
 };
 
+//Controller function to find cars older than 5 years
+const findByModel = async (req, res) => {//Define an async function to find cars older than 5 years from the database
+    try {
+        // Use the Car model to find cars where the 'model' field is less than or equal to 2018
+        const cars = await Car.find({model: {$lte: 2018}})
+       
+        //Conditional rendering to check if there are no cars found
+        if (!cars || cars.length === 0) {
+            // Respond with a 404 status code and a JSON object indicating no cars were found
+            return res.status(404).json({error: "No cars found"});
+        }
+
+        res.json(cars); // Respond with a JSON array of cars that are older than 5 years
+
+
+
+    } catch (error) {
+        //Handle errors
+        console.error('error finding cars', error.message); //Log error message in the console for debugging puposes
+        res.status(500).send('Internal server Error');// Respond with a 500 (Internal server error) status code and an error message
+    }
+
+    
+
+
+}
+
 // Export the functions so that they can be used in other parts of the application
 module.exports = {
-  addCar,
-  findAllCars,
-  updateById,
-  removeById
+    addCar,
+    findAllCars,
+   updateById,
+   removeById,
+   findByModel
 };
