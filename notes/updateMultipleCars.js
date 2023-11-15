285af8d2fc7b1f8e72b0a5a25039de8855554bb3
laraklopper <135839853+laraklopper@users.noreply.github.com>
2023-11-15 09:19:30 +0200

    Update updateMultipleCars.js

--- notes/updateMultipleCars.js
+++ notes/updateMultipleCars.js
@@ -1,54 +1,35 @@
-const updateMultipleCars = async (req, res) => {
+const updateMultipleCars = async (req, res) => {//Define an async function to update multiple cars
     try {
         // Extract old and new values from the request body
-        const oldMake = req.body.make;
-        const newMake = req.body.newMake;
-        const oldModel = req.body.model;
-        const newModel = req.body.newModel;
-        const oldRegistration = req.body.registration;
-        const newRegistration = req.body.newRegistration;
-        const oldOwner = req.body.owner;
-        const newOwner = req.body.newOwner;
+        const oldOwner = req.body.owner
+        const newOwner = req.body.newOwner
+     
+        // Log extracted values for debugging purposes
+        console.log('Old Owner:', oldOwner);
+        console.log('New Owner:', newOwner);
 
-        console.log(oldMake);
-        console.log(newMake);
-        console.log(oldModel);
-        console.log(newModel);
-        console.log(oldRegistration);
-        console.log(newRegistration);
-        console.log(oldOwner);
-        console.log(newOwner);
 
-        // Update multiple cars using updateMany
-        const updatedCars = await Car.updateMany(
-            // Specify the filter criteria for the documents to update
-            {
-                make: oldMake,
-                model: oldModel,
-                registration: oldRegistration,
-                owner: oldOwner
-            },
-            {
-                // Use $set to update the specified fields with the new values from req.body
-                $set: {
-                    make: newMake,
-                    model: newModel,
-                    registration: newRegistration,
-                    owner: newOwner
-                }
-            }
+        // Update the cars based on the filter criteria
+        const updatedCar = await Car.updateMany(
+            { owner: oldOwner }, // Use the variable oldOwner here
+            { $set: { owner: newOwner } },
+            { new: true } // This option is not necessary for updateMany
         );
 
-        if (!updatedCars) {
+        //Conditional rendering to check if the car is found
+        if (!updatedCar) {
             // If no cars were found for update, respond with a 404 status code and an error message
             return res.status(404).json({ error: 'No cars found for update' });
         }
 
-        // Respond with a JSON object containing the updated cars
+        // Find and return the updated cars
+        const updatedCars = await Car.find({ owner: newOwner });
         res.json(updatedCars);
-    } catch (error) {
+
+    }
+   catch (error) {
         // Handle errors
-        console.error('Error updating cars', error.message);
-        res.status(500).send({ error: 'Internal server error' });
+        console.error('Error updating cars', error.message);//Log error message in the console for debugging puposes
+        res.status(500).send({ error: 'Internal server error' });// Respond with a 500 (Internal Server Error) status code and an error message
     }
 };
